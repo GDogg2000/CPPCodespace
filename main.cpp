f5856cee43a71b5f1c024dc0e75cad87b0340f8d
GDogg2000 <ferencko@yahoo.com>
2024-05-28 03:24:18 +0000

    Updated the Program

--- main.cpp
+++ main.cpp
@@ -1,9 +1,21 @@
+/*
+  Escape sequences inside of string literals
+
+  \n newline \t tab \" double quote \\ backslash
+  
+*/
+
 #include <iostream>
 
 using namespace std;
 
 int main() {
-  cout << "Hello C++ Coedspace" << endl;
+  cout << "Hello" << endl;
+  cout << "Hello\n";
+  cout << "\"Hello\"" << endl;
+  cout << "\\n means newline\n\n";
+  cout << "12345678901234567891234567890" << endl;
+  cout << "1\t2\t3\n\t1\t2\t3" << endl;
 
   return EXIT_SUCCESS;
 }
